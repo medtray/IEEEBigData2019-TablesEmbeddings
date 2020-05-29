63b57048d27153ecca5b05dec875549a4e422554
Mohamed Trabelsi <med.trabelsi.ept@gmail.com>
2020-05-29 01:50:52 -0400

    Delete gpu_example.py

--- gpu_example.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-tf.compat.v1.disable_v2_behavior()
-print('tf version = ', tf.__version__)
-with tf.device('/gpu:0'):
-    a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
-    b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
-    c = tf.matmul(a, b)
-with tf.compat.v1.Session() as sess:
-    print (sess.run(c))
